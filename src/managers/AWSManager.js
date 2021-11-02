e0d399c757e59f6a4877af170ac733a17364bf64
wook <nam1260@gmail.com>
2021-11-03 00:04:32 +0900

    [dev_moment] aws api manager 초기 커밋

---
+++ src/managers/AWSManager.js
@@ -0,0 +1,40 @@
+/**
+ * AWSAPIManager
+ * @author wook
+ * @since 2021/10/31
+ * description
+ * AWS 연동 API 명세
+ */
+
+import axios from "axios"
+
+const SERVER_URL  =  "https://8wuahwyzk9.execute-api.ap-northeast-2.amazonaws.com/test";
+const API_KEYS = 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL';
+const headers = {
+    'x-api-key' : 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL'
+}
+
+const AWSManager = (function() {
+
+    const reqUserInfo = async (props) => {
+
+        const url = SERVER_URL + "/reg-user-info";
+
+        return await axios.post(url,{
+
+            params: {
+                userId: props.userId,
+                userNm: props.userNm,
+                userNickNm: props.userNickNm,
+                phoneNum: props.phoneNm,
+            }
+
+        },{headers})
+
+
+
+    };
+
+
+
+}());
