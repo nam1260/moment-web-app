b00de6ec83c5d0e7fadee4a51d93a92b00e39d13
sw.nam <nam1260@gmail.com>
2021-11-03 15:22:22 +0900

    AWS 매니저 수정

--- src/managers/AWSManager.js
+++ src/managers/AWSManager.js
@@ -11,23 +11,23 @@ import axios from "axios"
 const SERVER_URL  =  "https://8wuahwyzk9.execute-api.ap-northeast-2.amazonaws.com/test";
 const API_KEYS = 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL';
 const headers = {
-    'x-api-key' : 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL'
+    // 'x-api-key' : 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL',
+    // //'Access-Control-Allow-Origin' : '*'
+    // 'access-control-allow-origin' : '*'
 }
 
 const AWSManager = (function() {
 
-     const reqUserInfo = async (props) => {
+     const reqUserInfo = async (params) => {
 
-        const url = SERVER_URL + "/reg-user-info";
-
+        const url = SERVER_URL + "/reqUserInfo";
+  
         return await axios.post(url,{
 
-            params: {
-                userId: props.userId,
-                userNm: props.userNm,
-                userNickNm: props.userNickNm,
-                phoneNum: props.phoneNm,
-            }
+            userId: params ? params.userId : "testUserId",
+            userNm: params ? params.userNm : "testUserId",
+            userNickNm: params ? params.userNickNm : "testUserId",
+            phoneNum: params ? params.phoneNum : "testUserId",
 
         },{headers})
 
