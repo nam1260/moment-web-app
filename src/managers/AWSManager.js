bff277b6d382c9ff8e40b1fdd2b795d1c25336d0
sw.nam <nam1260@gmail.com>
2021-11-03 19:04:22 +0900

    [moment_dev] aws api 경로 재설정 및 api 추가

--- src/managers/AWSManager.js
+++ src/managers/AWSManager.js
@@ -8,23 +8,21 @@
 
 import axios from "axios"
 
-const SERVER_URL  =  "https://8wuahwyzk9.execute-api.ap-northeast-2.amazonaws.com/test";
+const SERVER_URL  =  "https://8wuahwyzk9.execute-api.ap-northeast-2.amazonaws.com/test/user";
 const API_KEYS = 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL';
 const headers = {
-    // 'x-api-key' : 'hFkmbKrQxO7G8EyATQbBQ8UP8qaS2Lru3ndYbHWL',
-    // //'Access-Control-Allow-Origin' : '*'
-    // 'access-control-allow-origin' : '*'
+    'x-api-key' : API_KEYS,
 }
 
 const AWSManager = (function() {
 
      const reqUserInfo = async (params) => {
 
-        const url = SERVER_URL + "/reqUserInfo";
+        const url = SERVER_URL + "/user/regUserInfo";
   
         return await axios.post(url,{
 
-            userId: params ? params.userId : "testUserId",
+            userId: params ? params.userId : "testUserId2",
             userNm: params ? params.userNm : "testUserId",
             userNickNm: params ? params.userNickNm : "testUserId",
             phoneNum: params ? params.phoneNum : "testUserId",
