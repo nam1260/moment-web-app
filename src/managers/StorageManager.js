9a19713c79b836d946dfdf2ed2385934f1c48c4d
wook <nam1260@gmail.com>
2021-11-05 22:28:23 +0900

    [moment_dev] storageManager localString key 로드/세이브/리무브 로직 추가

--- src/managers/StorageManager.js
+++ src/managers/StorageManager.js
@@ -7,8 +7,70 @@
  * 로그인 토큰 및 세션 관리용 매니저,
  */
 
-const StorageManager = (function() {
+export const StorageManager = (function() {
+
+    let localStorage;
+
+    let CACHED_KEYS = [
+        "key.user_token"
+    ];
+
+    let cachedStorage = [];
+    const getLocalStorage = function() {
+
+        if(!localStorage) {
+            localStorage = window.localStorage;
+        }
+
+        return localStorage;
+    };
+
+    const _load  = function(key) {
+
+        let index = CACHED_KEYS.indexOf(key);
+        let value;
+
+        if (index !== -1) {
+            value = cachedStorage[index];
+        }
+
+        if(!value) {
+            value = getLocalStorage().getItem(key);
+        }
+        return value
+    };
+
+    const _save = function (key, value) {
+
+        let index = CACHED_KEYS.indexOf(key);
+
+        try {
+            getLocalStorage().setItem(key, value);
+
+            //cached storage 저장
+            index = CACHED_KEYS.indexOf(key);
+            if (index !== -1) {
+                cachedStorage[index] = value;
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const _remove = function(key) {
+            getLocalStorage().removeItem(key);
+            //cached storage 삭제
+        let index = CACHED_KEYS.indexOf(key);
+        if (index !== -1) {
+            cachedStorage[index] = null;
+        }
+    };
+
+    return {
+        load: _load,
+        save: _save,
+        remove: _remove
+    }
+
 
 }());
-
-export default StorageManager;
---
+++ src/resources/Config.js
@@ -0,0 +1,17 @@
+/**
+ * Config
+ * @author wook
+ * @since 2021/11/05
+ * description
+ */
+
+export const CONFIG = {};
+
+
+Object.defineProperty(CONFIG, "STORAGE_KEY", {
+    value: {
+        "USER_TOKEN": "key.user_token"
+    },
+    writable : true
+});
+
