a3e1ad393a8a74f4e4562c513bc988ab8fff08a4
wook <nam1260@gmail.com>
2021-12-19 16:02:30 +0900

    fix(moment_dev): 검색 기능 blocked

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -3,11 +3,14 @@ import { useHistory } from 'react-router';
 import '../../Common.css';
 import './header.css';
 import { WrapLoginedComponent } from '../../shared/component/common/WrapLoginedComponent';
+import { message } from "antd";
 
 const menuPath = '/assets/icons/icoMenu.png'
 const searchPath = '/assets/icons/icoSearch.png'
 
 const logoPath = '/assets/images/logo.png'
+
+
 
 
 function Header({ setIsMenuOpen, isLogined, userNickNm, userId }) {
@@ -35,7 +38,9 @@ function Header({ setIsMenuOpen, isLogined, userNickNm, userId }) {
                     <img alt="none" onClick={() => history.push('/')} className={'top-logo'} src={logoPath} />
                 </div>
                 <div>
-                    <img alt="none" className={"top-icon"} src={searchPath} onClick={()=> { history.push('/search') }} />
+                    <img alt="none" className={"top-icon"} src={searchPath} onClick={()=> {
+                        message.warn("서비스 준비중입니다",1); return ;history.push('/search')
+                    }} />
                     <img alt="none" className={"top-icon"} src={loginPath} onClick={onClickProfile}/>
                 </div>
             </div>
