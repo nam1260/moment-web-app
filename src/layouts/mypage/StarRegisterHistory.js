9caf1d5b2f56355782f44f5396751d7be2a7a4e7
hs.kim3 <hs.kim3@altimedia.kr>
2022-01-02 22:14:32 +0900

    [moment_dev] 스타 등록 현황 버튼 동작 추ㅏ

--- src/layouts/mypage/StarRegisterHistory.js
+++ src/layouts/mypage/StarRegisterHistory.js
@@ -17,10 +17,20 @@ const regBodyStatus = {
     IN_PROGRESS:2,
 }
 
+const MODAL_TYPE = {
+    INIT: 0,
+    BUTTON: 1,
+    INPUT:2,
+    NOTI:3,
+}
+
 function StarRegisterHistory({isLogined}) {
     const history = useHistory();
     const [bodyStatus, setBodyStatus] = useState(regBodyStatus.INIT);
     const [starRegInfo, setStarRegInfo] = useState({});
+    const [showModal, setShowModal] = useState(false);
+    const [modalType, setModalType] = useState(MODAL_TYPE.INIT);
+    const textareaElement = useRef();
 
     const NotFoundComponent = () => {
         console.log('NotFoundComponent');
@@ -35,24 +45,24 @@ function StarRegisterHistory({isLogined}) {
         let buttonCancel = (
             <button className="normal" onClick={()=>{
                     console.log('buttonCancel');
-                    let userId = StorageManager.loadUserInfo() ? StorageManager.loadUserInfo().userId : "";
-                    AWSManager.cnclRgstStar({
-                        userId,
-                    }).then((result) =>{
-                        console.log("cnclRgstStar = " , result);
-                    });
+                    setModalType(MODAL_TYPE.BUTTON);
+                    setShowModal(true);
                 }
             }> 취소</button>);
         let buttonReply = (
             <button className="highlight"
                 onClick={()=>{
                     console.log('buttonReply');
+                    setModalType(MODAL_TYPE.INPUT);
+                    setShowModal(true);
                 }
             }> 답변입력</button>);
         let buttonReject = (
             <button className="full"
                 onClick={()=>{
                     console.log('buttonReject');
+                    setModalType(MODAL_TYPE.NOTI);
+                    setShowModal(true);
                 }
             }> 사유 확인</button>);
         let buttonAccept = (
@@ -80,6 +90,7 @@ function StarRegisterHistory({isLogined}) {
         "등록 완료",
     ];
     const FoundComponent = () => {
+        console.log('FoundComponent');
         return (
             <div className="message">
                 {
@@ -108,6 +119,98 @@ function StarRegisterHistory({isLogined}) {
                         </div>
                     </div>
                 }
+            </div>
+        )
+    };
+
+    const buttomModalComponent = () => {
+        console.log('buttomModalComponent');
+        return (
+            <div className="button_modal_short">
+                <div className="info_container">
+                    <br/>
+                    <span className="title">스타 등록 신청을</span>
+                    <span className="title">취소하시겠습니까?</span>
+                </div>
+                <div className="button_container">
+                    <button className="left_button" onClick={()=>{
+                            setShowModal(false);
+                        }}>
+                        아니요
+                    </button>
+                    <button className="right_button" onClick={()=>{
+                            let userId = StorageManager.loadUserInfo() ? StorageManager.loadUserInfo().userId : "";
+                            AWSManager.cnclRgstStar({
+                                userId,
+                            }).then((result) =>{
+                                console.log("cnclRgstStar = " , result);
+                            });
+                            setShowModal(false);
+                        }}>
+                        예
+                    </button>
+                </div>
+            </div>
+        )
+    };
+    const inputModalComponent = () => {
+        console.log('buttomModalComponent');
+        return (
+            <div className="button_modal_input">
+                <div className="info_container">
+                    <br/>
+                    <span className="title">DM으로 받은 답변을 입력해주세요.</span>
+                    <div className="write-wrapper">
+                            <textarea 
+                                onChange={(e) => {
+                                    const { name, value } = e.target;
+                                    const nextInputs = {
+                                        ...starRegInfo,  
+                                        [name]: value,
+                                    };
+                                    setStarRegInfo(nextInputs)}}
+                                value={starRegInfo.userComment}
+                                ref={textareaElement} 
+                                name="userComment"
+                            ></textarea>
+                        </div>
+                </div>
+                <div className="button_container">
+                    <button className="left_button" onClick={()=>{
+                            setShowModal(false);
+                        }}>
+                        취소
+                    </button>
+                    <button className="right_button" onClick={()=>{
+                            AWSManager.updateRgstStarStatus(starRegInfo).then((result) =>{
+                                console.log("cnclRgstStar = " , result);
+                            });
+                            setShowModal(false);
+                        }}>
+                        보내기
+                    </button>
+                </div>
+            </div>
+        )
+    };
+    const notiModalComponent = () => {
+        console.log('buttomModalComponent');
+        return (
+            <div className="button_modal_noti">
+                <div className="info_container">
+                    <br/>
+                    <span className="title">거절 사유:</span>
+                    <span className="title">{starRegInfo.adminComment}</span>
+                    <span className="description">문의는 아래 이메일을 참조해 주세요.</span>
+                    <span className="description">mtm.moment@gmail.com</span>
+                </div>
+                <div className="button_container">
+                    <button className="center_button" onClick={()=>{
+                            setShowModal(false);
+                        }}>
+                        확인
+                    </button>
+                </div>
             </div>
         )
     };
@@ -146,6 +249,17 @@ function StarRegisterHistory({isLogined}) {
                             [regBodyStatus.IN_PROGRESS] : <FoundComponent />
                         }[bodyStatus]   
                     }
+                    {showModal ?
+                    <Modal setShowModal={setShowModal}>
+                        {
+                            {
+                                [MODAL_TYPE.INIT] :   <></>,
+                                [MODAL_TYPE.BUTTON] : buttomModalComponent(),
+                                [MODAL_TYPE.INPUT] : inputModalComponent(),
+                                [MODAL_TYPE.NOTI] : notiModalComponent(),
+                            }[modalType]
+                        }
+                    </Modal> : null}
                 </div>
                 <div>
                     <span class="loadingGuide">
