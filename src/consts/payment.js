4ade891e791a7430291730faba767117ecac9a75
sw.nam <nam1260@gmail.com>
2022-01-25 17:06:50 +0900

    fix[master] 동적커리 통일화를 위한 상태 값 string 으로 변경

--- src/consts/payment.js
+++ src/consts/payment.js
@@ -1,14 +1,14 @@
 export const PAYMENT_STATUS = {
-    BEFORE: 0,
-    COMPLETE: 1,
-    CANCEL: 2,
+    BEFORE: "0",
+    COMPLETE: "1",
+    CANCEL: "2",
 }
 
 export const PAYMENT_TYPE = {
-    NORMAL: 0,
-    TOSS: 1,
-    NAVER: 2,
-    KAKAO: 3,
+    NORMAL: "0",
+    TOSS: "1",
+    NAVER: "2",
+    KAKAO: "3",
 }
 
 export const MSG_STATUS = {
